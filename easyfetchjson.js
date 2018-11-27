62dc6d8afe128e6561fd3cbae4b206ec11349ca5
Mohd <fmohd195@gmail.com>
2018-11-27 19:49:47 +0530

    All HTTP Request Methods Finalised

--- easyfetchjson.js
+++ easyfetchjson.js
@@ -37,4 +37,70 @@
             .catch(error => reject(error));
         })
     }
+
+    //Make an HTTP POST request
+    post(url, data){
+        return new Promise((resolve, reject)=>{
+            fetch(url, {
+                method : 'POST',
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(response =>{
+                if(response.status!=201){
+                    throw new httpError(response);
+                }
+                else{
+                    return response.json();
+                }
+            })
+            .then((data)=> resolve(data))
+            .catch(error => reject(error));
+        })
+    }
+
+    //Make an HTTP PUT Request
+    put(url, data){
+        return new Promise((resolve, reject)=>{
+            fetch(url, {
+                method: 'PUT',
+                headers :{
+                    'Content-Type': 'application/json'
+                },
+                body : JSON.stringify(data)
+            })
+            .then((response) =>{
+                if(response.status!=200){
+                    throw new httpError(response);
+                }
+                else{
+                    return response.json();
+                }
+            })
+            .then(data => resolve(data))
+            .catch(error => reject(error))
+        });
+    }
+
+    //Make an HTTP Delete Request
+    delete(url){
+        return  new Promise((resolve, reject)=>{
+            fetch(url, {
+                method: "DELETE"
+            })
+            .then((response)=> {
+                if(response.status!=200){
+                    throw new httpError(response);
+                }
+                else{
+                    return response.json();
+                }
+            })
+            .then(data => resolve(`Deletion Successful :)`))
+            .catch(err => reject(error));
+        })
+    }
+
  }
